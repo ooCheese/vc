bb126923627c3af94387fc671c9d15beecc1cf73
Lukas <dr.kaese@live.com>
2020-05-06 21:02:25 +0200

    aufg 6

---
+++ a3/a6.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def main():
+    a = np.array([17,42])
+    b = np.array([289,68])
+
+    print("4 way neighborhood ",b[0] - a[0])
+    print("8 way neighborhood ",b[0] - a[0] + b[0] - a[0])
+    print("euclidean distance ",np.linalg.norm(a-b))
+
+
+if __name__ == "__main__":
+    main()
