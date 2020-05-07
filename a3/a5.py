cb0b338069efcf859fab29a4e8f441bc96065c14
Lukas <dr.kaese@live.com>
2020-05-07 12:56:56 +0200

    aufg5 erledigt

--- a3/a5.py
+++ a3/a5.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 def main():
-    rg = (1/2,1/4)
-    rgb_g = 1 - rg[0] - rg[1]
     
-    rgb_r = (rg[0]*rgb_g)/rg[1]
-    rgb_b = ((1 - rg[0] - rg[1])*rgb_g)/rg[0]
+    rg = (1/2,1/4)
+    rgb_g = 1
 
-    rgb = (rgb_r,rgb_g,rgb_b)
-    print(rgb)
+    rgb_r = (rg[0]*rgb_g)/rg[1]
+    rgb_b = ((1 - rg[0] - rg[1])*rgb_g)/rg[1]
+
+    rgb = np.array([rgb_r,rgb_g,rgb_b])
+    rgb = rgb/max(rgb)
+
+    print("RGB : ",rgb)
 
 if __name__ == "__main__":
     main()
