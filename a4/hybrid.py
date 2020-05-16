836ddd315cc7cb30ad96e39a5c636e3528a22aa2
Lukas <dr.kaese@live.com>
2020-05-16 15:51:52 +0200

    a3

--- a4/hybrid.py
+++ a4/hybrid.py
@@ -1,67 +1,94 @@
-import numpy as np 
+import numpy as np
+from matplotlib import pyplot
+import scipy
 import math
 import cv2
+
+
+OUT_DIR = "out/hybrid/"
 
 def fftshift(img):
     f = np.fft.fft2(img)
     fshift = np.fft.fftshift(f)
-    return np.log(np.abs(fshift))
-
-def fft(img):
-    f = np.fft.fft2(img)
-    return np.log(np.abs(f))
+    return fshift
 
 def ifft(img):
     f_ishift = np.fft.ifftshift(img)
     img_back = np.fft.ifft2(f_ishift)
     return np.real(img_back)
 
-def calcG(x,y,sigma):
-    return (1/(2*math.pi*sigma**2))*math.e**(-(x**2+y**2)/(2*sigma**2))
+def filter(img,sigma):
+    out = np.zeros(img.shape)
+    h = img.shape[0]
+    w = img.shape[1]
 
-def G(img,sigma):
-    n = np.copy(img)
-    for x in range(n.shape[0]):
-        for y in range (n.shape[1]):
-            n[x][y] = calcG(x,y,sigma)
-    return n
+    H = h // 2
+    W = w // 2
 
-def gen_hybrid(i_1,i_2,sigma):
-    #I_1 = fftshift(i_1)
-    #I_2 = fftshift(i_2)
+    A = 1/sigma
 
-    G_1 = cv2.GaussianBlur(i_1,(sigma,sigma),0)
-    G_2 = cv2.GaussianBlur(i_2,(sigma,sigma),0)
+    for x in range(h):
+        for y in range(w):
+            #out[x][y]= math.exp(-1.0*((H-x)**2 + (W-y)**2)/(A**2))
+            out[x][y] = (1/(2*math.pi*sigma**2))*math.e**(-1.0 * (((y - W)**2 + (x - H)**2)/(2 * sigma**2)))
+    return out
 
-    return i_1 * G_1 + i_2*(1-G_2)
+def gen_color_hybrid(i_1,i_2,sigma):
+    #split images in  b g r channels 
+    b1 , g1, r1 = cv2.split(i_1)
+    b2 , g2, r2 = cv2.split(i_2)
+
+    B = gen_hybrid(b1,b2,sigma,"colorFilter/hybrid_color_blue_filter")
+    G = gen_hybrid(g1,g2,sigma,"colorFilter/hybrid_color_green_filter")
+    R = gen_hybrid(r1,r2,sigma,"colorFilter/hybrid_color_red_filter")
+
+    return cv2.merge((B,G,R))
 
 
-def main():
-    i_1 = cv2.imread("resource/Marylin_grey.png",0) / 255 #gray
-    i_2 = cv2.imread("resource/John_grey.png",0) / 255 #gray
-    sigma = 11
+def gen_hybrid(i_1,i_2,sigma,name="hybrid"):
+    I_1 = fftshift(i_1)
+    I_2 = fftshift(i_2)
 
-    H_gray = gen_hybrid(i_1,i_2,sigma)
+    #G_1 = cv2.GaussianBlur(I_1,(sigma,sigma),0)
+    G_1 = filter(I_1,sigma) # low pass
+    G_1 = np.interp(G_1,(np.min(G_1),np.max(G_1)),(0,1))
+    G_2 = 1 - G_1 # high pass
 
-    cv2.imshow("hybrid gray",H_gray)
-    cv2.imwrite("out/hybrid_gray.png",H_gray* 255)
+    H1 = ifft(I_1 * G_1)
+    H2 = ifft(I_2 * G_2)
+
+    cv2.imshow("hybrid",H1)
+    cv2.imwrite(OUT_DIR+name+"_1.png",H1 * 255)
     cv2.waitKey(0)
 
-    #color hybrid
-    i_1 = cv2.imread("resource/pizza1.jpg") / 255 #color
-    i_2 = cv2.imread("resource/pizza2.jpg") / 255 #color
+    cv2.imshow("hybrid",H2)
+    cv2.imwrite(OUT_DIR+name+"_2.png",H2 * 255)
+    cv2.waitKey(0)
 
-    #resize
-    width = int(i_1.shape[1] / 10)
-    height = int(i_2.shape[0] / 10)
-    dim = (width, height)
-    i_1 = cv2.resize(i_1,dim)
-    i_2 = cv2.resize(i_2,dim)
+    H = I_1 * G_1 + I_2*G_2
+    return ifft(H)
 
-    H_col = gen_hybrid(i_1,i_2,sigma)
+def main():
+    np.set_printoptions(suppress=True)
+
+    i_1 = cv2.imread("resource/Marylin_grey.png",0) / 255 #gray
+    i_2 = cv2.imread("resource/John_grey.png",0) / 255 #gray
+    sigma = 21
+
+    H_gray = gen_hybrid(i_1,i_2,sigma,"GrayFilter/hybrid_gray_filter")
+
+    cv2.imshow("hybrid",H_gray)
+    cv2.imwrite(OUT_DIR+"hybrid_gray.png",H_gray * 255)
+    cv2.waitKey(0)
+
+    #color hybrid 
+    i_1 = cv2.imread("resource/horse.png") / 255 #color
+    i_2 = cv2.imread("resource/zebra.png") / 255 #color
+
+    H_col = gen_color_hybrid(i_1,i_2,sigma)
 
     cv2.imshow("hybrid",H_col)
-    cv2.imwrite("out/hybrid_color.png",H_col* 255)
+    cv2.imwrite(OUT_DIR+"hybrid_color.png",H_col * 255)
     cv2.waitKey(0)
     
 
